afea5511b2946ae634ae89e543462314bb4a3150
Dave Trost <davetrost@gmail.com>
2019-12-03 10:54:24 -0800

    file renaming

---
+++ challenges/day2-part1.py
@@ -0,0 +1,19 @@
+import math as math
+with open('./inputs/day2.txt', 'r') as f:
+  inputs = f.readline().split(',')
+
+for i in range(len(inputs)): 
+  inputs[i] = int(inputs[i])
+
+inputs[1] = 12
+inputs[2] = 2
+
+for i in range(0, len(inputs), 4): 
+  op, pos1, pos2, posResult = inputs[i:i+4]
+  if op == 99: break
+  if op == 1: 
+    inputs[posResult] = inputs[pos1] + inputs[pos2]
+  if op == 2: 
+    inputs[posResult] = inputs[pos1] * inputs[pos2]
+
+print(inputs)
--- challenges/day2.py
+++ challenges/day2.py
@@ -5,15 +5,20 @@ with open('./inputs/day2.txt', 'r') as f:
 for i in range(len(inputs)): 
   inputs[i] = int(inputs[i])
 
-inputs[1] = 12
-inputs[2] = 2
+for noun in range(99):
+  for verb in range(99):
+    tempList = inputs[0:len(inputs)]
+    tempList[1] = noun
+    tempList[2] = verb
 
-for i in range(0, len(inputs), 4): 
-  op, pos1, pos2, posResult = inputs[i:i+4]
-  if op == 99: break
-  if op == 1: 
-    inputs[posResult] = inputs[pos1] + inputs[pos2]
-  if op == 2: 
-    inputs[posResult] = inputs[pos1] * inputs[pos2]
-
-print(inputs)
+    for i in range(0, len(tempList), 4): 
+      op, pos1, pos2, posResult = tempList[i:i+4]
+      if op == 99: break
+      if op == 1: 
+        tempList[posResult] = tempList[pos1] + tempList[pos2]
+      if op == 2: 
+        tempList[posResult] = tempList[pos1] * tempList[pos2]
+    
+    if tempList[0] == 19690720:
+      print(noun, verb)
+      break
