53f42e4d50bed0206aed58e1dc9a1b021bb9824d
Dave Trost <davetrost@gmail.com>
2019-12-03 10:23:03 -0800

    day3 part1 er-write. WORKS!

--- challenges/day3-part1.py
+++ challenges/day3-part1.py
@@ -1,63 +1,61 @@
 with open('./inputs/day3.txt', 'r') as f:
-  directionsForWire1 = f.readline().split(',')
-  directionsForWire2 = f.readline().split(',')
+  instructionsForWire1 = f.readline().split(',')
+  instructionsForWire2 = f.readline().split(',')
 
-def coordinateAddition(a1, a2):
-  (x1, y1) = a1
-  (x2, y2) = a2
-  return [x1 + x2, y1 + y2]
+def getCoordsAndOffsets(instructions):
+  coordsList = []
+  (x, y) = [0, 0]
 
-# def cToStr(coord):
-#   (x, y) = coord
-#   return str(x) + ',' + str(y)
+  for instruction in instructions:
+    (dir, *dist) = list(instruction)  # convert string to list performs a split on every character
+    distance = int(''.join(dist))
 
-def listOfCoordsCrossed(current, instruction):
-  (x, y) = current
+    if dir == 'R': offset = [distance, 0]
+    if dir == 'L': offset = [-distance, 0]
+    if dir == 'U': offset = [0, distance]
+    if dir == 'D': offset = [0, -distance]
+    x += offset[0]
+    y += offset[1]
 
-  (dir, *dist) = list(instruction)
-  distance = int(''.join(dist))
+    coordsList.append({
+      'coord': [x, y],
+      'offset': offset,
+    })
 
-  offsetDict = {
-    'R': [1, 0],
-    'L': [-1, 0],
-    'D': [0, -1],
-    'U': [0, 1]
-  }
-  (dx, dy) = offsetDict[dir]
-
-  coords = [[x, y]]
-  for i in range(distance):
-    (x, y) = [x + dx, y + dy]
-    coords.append([x, y])
-
-  # modify the contents of current in-place
-  current[0] = x
-  current[1] = y
-  return coords
+  return coordsList
 
 # main
-current = [0, 0]
-coordinatesWire1 = []
-for instruction in directionsForWire1:
-  coordinatesWire1 += listOfCoordsCrossed(current, instruction)
-
-current = [0, 0]
-coordinatesWire2 = []
-for instruction in directionsForWire2:
-  coordinatesWire2 += listOfCoordsCrossed(current, instruction)
+coordsWire1 = getCoordsAndOffsets(instructionsForWire1)
+coordsWire2 = getCoordsAndOffsets(instructionsForWire2)
 
 crossingPoints = []
 min = 999999
-for coord1 in coordinatesWire1:
-  (x1, y1) = coord1
-  for coord2 in coordinatesWire2:
-    (x2, y2) = coord2
-    if(x1 == x2 and y1 == y2):
-      crossingPoints.append(coord1)
-      manhattanDist = abs(x1) + abs(y1)
-      if(manhattanDist < min and manhattanDist > 0): 
-        min = manhattanDist
-        print('new min', min, ' at ', coord1, coord2)
+for coord1 in coordsWire1:
+  (x1, y1) = coord1['coord']
+  (dx1, dy1) = coord1['offset']
+  for coord2 in coordsWire2:
+    (x2, y2) = coord2['coord']
+    (dx2, dy2) = coord2['offset']
+
+    if(bool(x1 - x2 > 0) != bool(x1 - dx1 - x2 > 0)):
+      if(bool(y2 - y1 > 0) != bool(y2 - dy2 - y1 > 0)):
+        print('crossing x: ', coord1, coord2)
+
+        crossingPoints.append([x2, y1])
+        manhattanDist = abs(x2) + abs(y1)
+        if(manhattanDist < min and manhattanDist > 0): 
+          min = manhattanDist
+          print('new min', min, ' at ', x2, y1)
+    
+    if(bool(y1 - y2 > 0) != bool(y1 - dy1 - y2 > 0)):
+      if(bool(x2 - x1 > 0) != bool(x2 - dx2 - x1 > 0)):
+        print('crossing y: ', coord1, coord2)
+
+        crossingPoints.append([x1, y2])
+        manhattanDist = abs(x1) + abs(y2)
+        if(manhattanDist < min and manhattanDist > 0): 
+          min = manhattanDist
+          print('new min', min, ' at ', x1, y2)
 
 print(crossingPoints)
 print(min)
